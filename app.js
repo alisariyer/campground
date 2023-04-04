efe60bd0e482f25198840933dbdde607022676f9
Ali Sariyer <alisariyer@gmail.com>
2023-04-04 12:07:33 +0200

    Add campground show route and view

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
+});
+
+// Specific campground route
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
 })
 
 // Run server
