d4542d8e70d546c18402cb09c089fd5712e6502c
Ali Sariyer <alisariyer@gmail.com>
2023-04-04 02:45:08 +0200

    Add two seed helper modules (some lists), seed db after deleting everything in index.js

---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+
+const main = async function () {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
+}
+
+// Connect to the database
+main()
+    .then(() => console.log("Connected to the DB"))
+    .catch(err => console.log("Error: ", err));
+
+// Get a random data for title
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    // Test db
+    // const c = new Campground({ title: 'Test camp'});
+    // await c.save();
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({ 
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+            })
+        await camp.save();
+    }
+
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
