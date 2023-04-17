9dd11f766d22688ae8cfc50387b0f4a35b95da73
Ali Sariyer <alisariyer@gmail.com>
2023-04-17 02:42:17 +0200

    Add partial footer, set body min-v-100, add image and desc. in db model and rebuild db

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,8 @@ const { Schema, model } = mongoose;
 
 const campgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     // await c.save();
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({ 
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://picsum.photos/200/300',
+            description: `Lorem ipsum image`,
+            price
             })
         await camp.save();
     }
