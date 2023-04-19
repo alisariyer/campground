7fbb5a788d4dd379ceaaed8ba4c8d24f823f78bb
Ali Sariyer <alisariyer@gmail.com>
2023-04-19 05:46:02 +0200

    Add a custom error class and some test error handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
+const AppError = require('./AppError');
 
 const main = async function () {
     await mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
@@ -23,10 +24,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'super') {
+        next();
+    } else {
+        // res.send('You cannot get this without password');
+        // res.status(401);
+        // throw new Error('Heyy password error');
+        throw new AppError('Hey no no no', 401);
+    }
+}
+
 // Setup routes
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+
+// Secret route
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('My secret is himmm. I cannot say...')
+})
+
+app.get('/error', (req, res) => {
+    hello.call();
+});
+
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not admin!!!', 403);
+})
 
 // Campgrounds route
 app.get('/campgrounds', async (req, res) => {
@@ -70,6 +97,18 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+});
+
+app.use((err, req, res, next) => {
+    // const message = `
+    // ***********************************************
+    // *****************   ERROR   *******************
+    // ***********************************************`
+    // console.log(message);
+    // next(err);
+    // res.status(500).send('We got an error!');
+    const { status = 500, message = 'Some error occured!!!' } = err;
+    res.status( status ).send(message);
 })
 
 // Run server
