49ca2b2aa6c072b319af2df404237a45138147b2
Ali Sariyer <alisariyer@gmail.com>
2023-05-10 00:29:26 +0200

    Add query middleware into campground schema to remove all reviwes after removing a campground

--- models/campground.js
+++ models/campground.js
@@ -1,16 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const review = require("./review");
 const { Schema, model } = mongoose;
 
 const campgroundSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String,
-    reviews: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-    }]
+  title: String,
+  image: String,
+  price: Number,
+  description: String,
+  location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
-module.exports = model('Campground', campgroundSchema);
+campgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    const deleteds = await review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+    console.log(deleteds);
+  }
+});
+
+module.exports = model("Campground", campgroundSchema);
